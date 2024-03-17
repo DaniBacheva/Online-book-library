99dc3d308b8d29214c1d794edeacb71d0bb8fab8
DaniBacheva <y.bacheva@abv.bg>
2024-03-17 22:33:49 +0200

    Register with reactive form

--- project/src/app/user/register/register.component.ts
+++ project/src/app/user/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { passwordMatchValidator } from 'src/app/validators/passwords-match';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group ({
+    username:["", [Validators.required]],
+    email:["", [Validators.required]],
+    passGroup: this.fb.group ({
+      password:["", [Validators.required]],
+      rePassword:["", [Validators.required]],
+    },
+    {
+    Validators:[passwordMatchValidator("password", "rePassword")]
+    }
+    ),
+    
+  })
 
+
+  constructor (private fb: FormBuilder, private userService:UserService,
+    private router:Router) {}
+
+    register() {
+      if (this.form.invalid){
+        return
+      }
+
+      const {
+        username, 
+        email, 
+        passGroup: {passwordMatchValidator, rePassword}={}
+    } = this.form.value;
+
+    console.log(this.form.value)
+    }
 }
---
+++ project/src/app/validators/passwords-match.ts
@@ -0,0 +1,18 @@
+import { FormGroup, ValidatorFn} from "@angular/forms"
+
+export function passwordMatchValidator(
+    passwordOne: string,
+    passwordTwo: string
+    ) : ValidatorFn {
+        return (control)=> {
+            const group = control as FormGroup;
+            const pass1 = group.get(passwordOne);
+            const pass2= group.get(passwordTwo);
+
+            return pass1?.value===pass2?.value
+            ? null 
+            : {passwordMatchValidator: true}
+        }
+
+
+    }
