2496620fb567c3468329fef1808d6e2d8563b8d9
DaniBacheva <y.bacheva@abv.bg>
2024-03-22 14:10:11 +0200

    home last three books

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import { Book } from '../types/book';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  booksList: Book[] | null = null;
+   
+  constructor (private apiService: ApiService,
+    private router: Router ) {}
 
+    ngOnInit(): void {
+      this.apiService.getAllBooks().subscribe( {
+        next:(books) => {
+          this.booksList = books.slice(-3);
+           console.log(this.booksList)
+        },
+        error: (err)=> {
+           console.log(err);
+        }
+       
+      })
+    }
+  
 }
